e2c1c38ff76aced5d11fdc6acfac4884620f4dc2
Adeel Younas <aedil12155@gmail.com>
2014-11-11 22:41:23 +0400

    master: added testcases, some enhancements

--- trimmer/models.py
+++ trimmer/models.py
@@ -10,39 +10,47 @@ cache = get_cache('default')
 
 
 class URLManager(models.Manager):
-    def suggest_url(self, url):
+    def create_url(self, url):
         """
         Match items for the given url, if none matches pick random one
-        from the list.
+        from the list, if all the words are used must reuse existing one,
+        must reuse in order ascending of when they were used first.
+        If given url already have tinyurl assigned to it, will return
+        existing.
         :param url: type URL
-        :return: group matched the url or random one
+        :return: TinyURL object
         """
         try:
             item = self.get(url=url)
         except ObjectDoesNotExist:
             pass
         else:
-            return item.word
+            return item
 
         all_words = list(self.filter(Q(url=None) | Q(url=""))
                          .values_list('word', flat=True))
 
         if not all_words:
-            url_obj = TinyURL.objects.order_by('updated')[0]
-            return url_obj.word
-
-        all_words.sort(key=len, reverse=True)
-        cached_regex = '|'.join(all_words)
-        regex = re.compile(cached_regex)
-        word = url.split("/")[-2]
-        match = regex.match(word)
-
-        if match:
-            group = match.group()
+            url_obj = self.order_by('updated')[0]
+            # return url_obj.word
         else:
-            group = all_words.pop()
+            all_words.sort(key=len, reverse=True)
+            cached_regex = '|'.join(all_words)
+            regex = re.compile(cached_regex)
+            word = url.split("/")[-2]
+            match = regex.match(word)
 
-        return group
+            if match:
+                group = match.group()
+            else:
+                group = all_words.pop()
+
+            url_obj = self.get(word=group)
+
+        url_obj.url = url
+        url_obj.save()
+
+        return url_obj
 
 
 class TinyURL(models.Model):
--- trimmer/tests.py
+++ trimmer/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from trimmer.models import TinyURL
 
-# Create your tests here.
+
+class TinyURLTestCase(TestCase):
+    def setUp(self):
+        self.words = ["tinyurl", "tinybit", "test", "bit"]
+        for word in self.words:
+            TinyURL.objects.create(word=word)
+
+    def test_generate_tiny_url(self):
+        """Test generating tiny url when empty words available,
+        reuse when all the available words are used"""
+        for word in self.words:
+            url = "http://www.web.com/%s/" % word
+            url_obj = TinyURL.objects.create_url(url=url)
+
+            self.assertEqual(url_obj.word, word)
+
+        # Test generating tiny url when all the words are used,
+        # should pick/reuse word that was used first
+        first_used = TinyURL.objects.order_by('updated')[0]
+
+        reuse_word = TinyURL.objects.create_url(
+            url="http://www.web.com/justtesting/")
+
+        self.assertEqual(reuse_word.word, first_used.word)
